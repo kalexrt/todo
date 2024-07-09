f62d9489b32f6ac28a55aa2173de65f86f67972e
Kalash Shrestha <69349296+kalexrt@users.noreply.github.com>
2024-07-09 09:40:26 +0545

    hotfix port for docker ccontainer

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
+const defaultPort = 3000;
 const config ={
-    port:process.env.PORT
+    port:process.env.PORT?process.env.PORT:defaultPort
 };
 export default config;
